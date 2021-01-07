1a5c505e23b167ce090f9ff9da2c7e7f6f5bbbb6
SilverMaiden <h.specials@gmail.com>
2021-01-06 23:18:35 -0800

    Fix CORS header issue, post request now working correctly

--- blend-blog-project/src/App.js
+++ blend-blog-project/src/App.js
@@ -1,12 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import FormikForm from './components/SignUp';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+<Route
+  {...rest}
+  render={props =>
+    localStorage.getItem("token") ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    )
+  }
+/>
+);
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home />
+      <br  />
+      <Switch>
+        <Route path="/" component={FormikForm} />
+        <PrivateRoute exact path='/home' component={Home} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
