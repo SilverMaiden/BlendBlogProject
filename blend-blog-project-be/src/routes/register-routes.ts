b593a5996e4048f76ca50b1e1df6539d79b9ac85
SilverMaiden <h.specials@gmail.com>
2021-01-10 13:13:33 -0800

    Add bcrypt library to backend

--- blend-blog-project-be/src/routes/register-routes.ts
+++ blend-blog-project-be/src/routes/register-routes.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import { RegisterUser } from '../models/register-model';
+import bcrypt from 'bcrypt'; // --More info on bcrypt library here: https://www.npmjs.com/package/bcrypt
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -11,13 +12,15 @@ router.use((req: Request, res: Response, next) => {
     res.header("Access-Control-Allow-Headers", "access-control-allow-origin, content-type, Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-// WHAT NEEDS TO HAPPEn
+// WHAT NEEDS TO HAPPEN
 
 /* I need to use this router for register. Let's make a function
 in router-model.ts that does everything we need.*/
 
 router.post('/', (req: Request, res: Response) => {
     Register.addUser(req.body)
+    // TODO - Passwords are not currently being hashed, and are therefore extremely insecure.
+    // Add bcrypt to hash password before adding it to DB
     .then((users: any) => {
         res.status(201).json(users);
     }).catch((err: any) => {
